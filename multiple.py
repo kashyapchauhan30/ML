67fbfd7d689b0b28f27c0a762e01951406d7bc1a
kashyapchauhan30 <chauhankashyap30@gmail.com>
2025-06-23 12:35:22 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,19 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+diabetes_model=pickle.load(open('D:/internship/sav_files/diabetes_model','rb'))
+
+heart_model=pickle.load(open('D:/internship/sav_files/Hear_model','rb'))
+
+parkinsons_model=pickle.load(open('D:/internship/sav_files/parkinson_model','rb'))
+
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                           
+                           ['Diabetes Prediction',
+                            'Heart Diseases Prediction',
+                            'Parkinsons Prediction'],
+                           icons=['activity','heart','person'],
+                           default_index=0)
