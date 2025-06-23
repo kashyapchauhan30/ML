ad90a3751399a20a26110365992ff74db6a50e35
kashyapchauhan30 <chauhankashyap30@gmail.com>
2025-06-23 12:36:50 +0530

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -2,11 +2,11 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model=pickle.load(open('D:/internship/sav_files/diabetes_model','rb'))
+diabetes_model=pickle.load(open('diabetes_model','rb'))
 
-heart_model=pickle.load(open('D:/internship/sav_files/Hear_model','rb'))
+heart_model=pickle.load(open('Hear_model','rb'))
 
-parkinsons_model=pickle.load(open('D:/internship/sav_files/parkinson_model','rb'))
+parkinsons_model=pickle.load(open('parkinson_model','rb'))
 
 with st.sidebar:
     
